0b4a0d0304f881e8f0b08856fb254f3f4aa7bd81
sami800435 <samiaakhand509@gmail.com>
2024-11-17 10:43:46 -0800

    Add files via upload

---
+++ islamic university/pera.js
@@ -0,0 +1,17 @@
+// document.write("Basify");
+console.log('Basify');
+// var x =4;
+// console.log(x)
+// let y =10;
+// console.log(y)
+
+// console log(o)
+
+let x =10 ;
+let y=1;
+
+if (x==10) {
+    
+} else {
+    
+}
